1187af9ccc2bccc15b85c3b8afe90322836f400c
htetarkar soe <89314908+geohtet@users.noreply.github.com>
2022-03-14 15:51:11 +0630

    Add files via upload

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ __date__ = '2022-02-12'
 __copyright__ = '(C) 2022 by Htet Arkar Soe'
 
 
-DOC_PLUGIN_URL = ('https://github.com/geohtet/Noise-Prediction-Algorithm/blob/872707950f999286e502c09977b35d511eb71b41/README.md')
+DOC_PLUGIN_URL = ('https://github.com/geohtet/Noise-Prediction-Algorithm/blob/main/README.md')
 
